8e06a78945936590b1401f8064693408e472e30a
Victor Masson <victormasson21@gmail.com>
2019-09-25 15:00:20 +0100

    Form takes input and prints it back

--- src/components/inputForm/index.js
+++ src/components/inputForm/index.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 
-const InputForm = () => {
+const InputForm = (props) => {
+  const [name, setName] = useState("gminova")
+
+
   return (
-    <div className="input-form__container">
-      <p>INPUT FORM!</p>
-    </div>
+    <form className="input-form">
+      <h1>Input form</h1>
+      <label className="input-form__label" htmlFor="input-form__input">Enter any Github handle</label>
+      <input
+        type="text"
+        className="input-form__input"
+        value={props.name}
+        onChange={e => setName(e.target.value)}
+      />
+      <h3>Current name: {name}</h3>
+    </form>
+
   );
 };
 

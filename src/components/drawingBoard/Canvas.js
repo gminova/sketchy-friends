03fa8daf01ef9d4f0ce0d6b86c2afe91b4372383
Gigi Minova <minova.gigi@gmail.com>
2019-09-25 18:52:28 +0100

    add tiny dog picture

--- src/components/drawingBoard/Canvas.js
+++ src/components/drawingBoard/Canvas.js
@@ -1,25 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./style.css";
 
 const Canvas = () => {
-  //mouse coordinates
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  useEffect(() => {
-    const handleMouseMove = event => {
-      setX(event.x);
-      setY(event.y);
-    };
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, []);
+  // //set flag for drawing event
+  // let drawing = false;
+  // const canva = document.querySelector(".drawing-board__canvas");
+  // //const context = canva.getContext("2d");
+  // //mouse coordinates
+  // const [x, setX] = useState(0);
+  // const [y, setY] = useState(0);
+  // useEffect(() => {
+  //   const handleMouseMove = event => {
+  //     setX(event.x);
+  //     setY(event.y);
+  //     drawing = true;
+  //   };
+  //   window.addEventListener("mousemove", handleMouseMove);
+  //   return () => {
+  //     window.removeEventListener("mousedown", handleMouseMove);
+  //   };
+  // }, []);
+  const canvasRef = useRef(null);
   return (
     <div>
-      <h2>Your mouse coordinates:</h2>
-      <pre>
-        {x}:{y}
-      </pre>
-      <canvas className="drawing-board__canvas"></canvas>
+      <canvas
+        className="drawing-board__canvas"
+        ref={canvasRef}
+        width={650}
+        height={400}
+        onClick={e => {
+          const canvas = canvasRef.current;
+          const ctx = canvas.getContext("2d");
+          alert(canvas);
+          // implement draw on ctx here
+        }}
+      ></canvas>
+      <img 
+      src="images/dog.jpeg" 
+      alt="dog-photo"
+      width={50}/>
     </div>
   );
 };

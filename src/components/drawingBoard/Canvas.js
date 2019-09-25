deb9ab572f58b15f84de910382a2ac39a0a03480
Gigi Minova <minova.gigi@gmail.com>
2019-09-25 23:18:50 +0100

    canva implemented

--- src/components/drawingBoard/Canvas.js
+++ src/components/drawingBoard/Canvas.js
@@ -1,44 +1,70 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 
 const Canvas = () => {
-  // //set flag for drawing event
-  // let drawing = false;
-  // const canva = document.querySelector(".drawing-board__canvas");
-  // //const context = canva.getContext("2d");
-  // //mouse coordinates
-  // const [x, setX] = useState(0);
-  // const [y, setY] = useState(0);
-  // useEffect(() => {
-  //   const handleMouseMove = event => {
-  //     setX(event.x);
-  //     setY(event.y);
-  //     drawing = true;
-  //   };
-  //   window.addEventListener("mousemove", handleMouseMove);
-  //   return () => {
-  //     window.removeEventListener("mousedown", handleMouseMove);
-  //   };
-  // }, []);
-  const canvasRef = useRef(null);
+  useEffect(() => {
+    // if user is drawing)mousedown) this will be set to true, else false (mouseup)
+    let isDrawing = false;
+    //starting mouse coordinates
+    let x = 0;
+    let y = 0;
+    // in order to select the HTML DOM element we need
+    // to have at least performed the first render on the DOM.
+    // The myPics will be null if the first render has not taken place
+    const myPics = document.querySelector(".drawing-board__canvas");
+    // Get the canvas context - we will draw on this and call functions to draw on the context
+    const context = myPics.getContext("2d");
+
+
+    // The x and y offset of the canvas from the edge of the page
+    const rect = myPics.getBoundingClientRect();
+
+    // Add the event listeners for mousedown, mousemove, and mouseup
+    myPics.addEventListener("mousedown", e => {
+      x = e.clientX - rect.left;
+      y = e.clientY - rect.top;
+      isDrawing = true;
+    });
+
+    // Add an event listener when the mouse if moved
+    myPics.addEventListener("mousemove", e => {
+      if (isDrawing === true) {
+        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+      }
+    });
+    // When the mouse up takes place we need to stop drawing
+    window.addEventListener("mouseup", e => {
+      if (isDrawing === true) {
+        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+      }
+    });
+
+    // core drawing function - takes the mouse coordinates and the context
+    // and draws from one coordinate point [x,y] to another [x,y]
+    function drawLine(context, x1, y1, x2, y2) {
+      context.beginPath();
+      context.strokeStyle = "black";
+      context.lineWidth = 1;
+      context.moveTo(x1, y1);
+      context.lineTo(x2, y2);
+      context.stroke();
+      context.closePath();
+    }
+  }, []);
+
+  
   return (
     <div>
       <canvas
         className="drawing-board__canvas"
-        ref={canvasRef}
         width={650}
         height={400}
-        onClick={e => {
-          const canvas = canvasRef.current;
-          const ctx = canvas.getContext("2d");
-          alert(canvas);
-          // implement draw on ctx here
-        }}
       ></canvas>
-      <img 
-      src="images/dog.jpeg" 
-      alt="dog-photo"
-      width={50}/>
     </div>
   );
 };

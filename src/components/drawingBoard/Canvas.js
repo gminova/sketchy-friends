461b26214b7591f0bc37b4fe270d3852408191dd
Gigi Minova <minova.gigi@gmail.com>
2019-09-25 15:21:08 +0100

    handle mouse coordinates

--- src/components/drawingBoard/Canvas.js
+++ src/components/drawingBoard/Canvas.js
@@ -2,9 +2,23 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 
 const Canvas = () => {
+  //mouse coordinates
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  useEffect(() => {
+    const handleMouseMove = event => {
+      setX(event.x);
+      setY(event.y);
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
   return (
     <div>
-      <h2>Draw here:</h2>
+      <h2>Your mouse coordinates:</h2>
+      <pre>
+        {x}:{y}
+      </pre>
       <canvas className="drawing-board__canvas"></canvas>
     </div>
   );

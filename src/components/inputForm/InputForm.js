efbef13035136a0fa0e5d508512534bef38dd520
Gigi Minova <minova.gigi@gmail.com>
2019-09-26 15:18:13 +0100

    tests

--- src/components/inputForm/InputForm.js
+++ src/components/inputForm/InputForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import {setProfile} from "./display";
 
 const InputForm = props => {
   return (
--- src/components/inputForm/display.test.js
+++ src/components/inputForm/display.test.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import Display from "./Display";
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Display from "./Display";
 
-// ensures our document gets cleared out after each test
-// so we don't have lots of copies of our component in there
-// otherwise our tests might affect each other
+// ensures our document gets cleared out after each test
+// so we don't have lots of copies of our component in there
+// otherwise our tests might affect each other
 afterEach(cleanup);
 
-test("Display component renders", () => {
-  const { getByAltText } = render(<Display />);
-  getByAltText("some image");
+test("Display component renders", () => {
+  const { getByAltText } = render(<Display />);
+  getByAltText("some image");
 });

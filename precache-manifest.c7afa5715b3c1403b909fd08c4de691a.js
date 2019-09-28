b9997778c5962bf0336abcf6904f81dbfb252a28
Gigi Minova <minova.gigi@gmail.com>
2019-09-28 09:43:32 +0100

    Updates

---
+++ precache-manifest.c7afa5715b3c1403b909fd08c4de691a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da3285e0c656743f086366e20f6e7d6",
+    "url": "/sketchy-friends/index.html"
+  },
+  {
+    "revision": "17831d26f6402bc55428",
+    "url": "/sketchy-friends/static/css/main.06655ec6.chunk.css"
+  },
+  {
+    "revision": "7b23b0e44965dba6e863",
+    "url": "/sketchy-friends/static/js/2.2ff26bcb.chunk.js"
+  },
+  {
+    "revision": "17831d26f6402bc55428",
+    "url": "/sketchy-friends/static/js/main.b3ca18ac.chunk.js"
+  },
+  {
+    "revision": "9f8e0341376c723a08ea",
+    "url": "/sketchy-friends/static/js/runtime-main.cbb04d1c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed6453414901723bacb0d3471bf4e013.js"
+  "/sketchy-friends/precache-manifest.c7afa5715b3c1403b909fd08c4de691a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sketchy-friends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
